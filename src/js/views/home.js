42a54f983282a677079b0316d2719253936be1ac
manuelgerdel <manuelgerdelm@gmail.com>
2023-08-02 10:03:50 -0400

    version with home correction

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,15 +4,12 @@ import "../../styles/home.css";
 import { Link } from "react-router-dom";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Contact list project</h1>
+    <div className="text-center mt-5">
+        <h1>Contact list project</h1>
 
-		<p>This project is basically a contact list creator. You can add, delete or edit some data about each one. Hope you enjoy it! </p>
+        <p>This project is basically a contact list creator. You can add, delete or edit some data about each one. Hope you enjoy it! </p>
 
-		<Link to="/form"><button type="button" className="btn btn-primary"> Create Contact</button></Link>
-	</div>
+        <Link to="/form"><button type="button" className="btn btn-primary"> Create Contact</button></Link>
+    </div>
 );
 
-
-
-// NO ESTAS TRABAJANDO EN LA MASTER BRANCH, UNE LOS CAMBIOS
